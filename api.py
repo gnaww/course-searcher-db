bf7f79f26435abb5119f412d843d2bf1eeefe4e7
dchen <chendavid96@gmail.com>
2016-10-05 17:32:02 -0400

    updated api.py

--- api.py
+++ api.py
@@ -15,20 +15,35 @@ class Courses(Resource):
         conn = e.connect()
         # Perform query and return JSON data
         query = conn.execute("select * from Fall_2016_SOC")
-        return {'courses': [i[0] for i in query.cursor.fetchall()]}
+        return {'courses': [i for i in query.cursor.fetchall()]}
 
 
 class CoursesGivenSubject(Resource):
     def get(self, subject_num):
+        s = subject_num.zfill(3)
+
         conn = e.connect()
-        query = conn.execute("select * from Fall_2016_SOC where course_subject='%s'" % subject_num)
+        query = conn.execute("select * from Fall_2016_SOC where course_subject='%s'" % s)
         # Query the result and get cursor.Dumping that data to a JSON is looked by extension
         result = {'courses': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return result
         # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
 
-api.add_resource(Courses, '/courses')
-api.add_resource(CoursesGivenSubject, '/courses/<string:subject_num>')
+class SectionsGivenSubjectAndCourse(Resource):
+    def get(self, subject_num, course_num):
+        s = subject_num.zfill(3)
+        c = course_num.zfill(3)
+
+        conn = e.connect()
+        query = conn.execute("select * from Fall_2016_SOC where course_subject='%s' and course_number='%s'" % (s, c))
+        # Query the result and get cursor.Dumping that data to a JSON is looked by extension
+        result = {'courses': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return result
+        # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
+
+api.add_resource(Courses, '/subject=GMA')
+api.add_resource(CoursesGivenSubject, '/subject=<string:subject_num>')
+api.add_resource(SectionsGivenSubjectAndCourse, '/subject=<string:subject_num>&course=<string:course_num>')
 
 
 if __name__ == '__main__':
